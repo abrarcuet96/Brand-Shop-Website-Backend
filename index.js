8f8a9ccc888df03f8bcd1228478ca8af1802a898
abrarcuet96 <133400726+abrarcuet96@users.noreply.github.com>
2023-10-19 23:30:20 +0600

    Create and Read opeation is done and database is saved in mongodb and shown to the server website as json

--- index.js
+++ index.js
@@ -6,9 +6,63 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res)=>{
+
+// abrarhaider1357
+// UCiQ9fa8AWOPench
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://abrarhaider1357:UCiQ9fa8AWOPench@cluster0.0zifi4k.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // -----------------------------------------------------------------------------------------------------
+        const productCollection= client.db("productsDB").collection("products");
+
+        // CREATE:
+        app.post('/products', async(req,res)=>{
+            const product= req.body;
+            const result= await productCollection.insertOne(product);
+            res.send(result);
+        });
+        app.get('/products', async(req,res)=>{
+            const result= await productCollection.find().toArray();
+            res.send(result);
+        });
+
+
+
+
+
+
+
+
+        // -----------------------------------------------------------------------------------------------------
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send('Brand-Shop server is running');
 });
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server is running on port: ${port}`);
 });
