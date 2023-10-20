c864e6da4ec66383be26479a174ebeba74390191
abrarcuet96 <133400726+abrarcuet96@users.noreply.github.com>
2023-10-20 16:42:28 +0600

    update operation is done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // UCiQ9fa8AWOPench
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://abrarhaider1357:UCiQ9fa8AWOPench@cluster0.0zifi4k.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,18 +28,56 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // -----------------------------------------------------------------------------------------------------
-        const productCollection= client.db("productsDB").collection("products");
+        const productCollection = client.db("productsDB").collection("products");
+        const cartProductCollection = client.db("productsDB").collection("cartProducts");
 
         // CREATE:
-        app.post('/products', async(req,res)=>{
-            const product= req.body;
-            const result= await productCollection.insertOne(product);
+        // Products:
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
             res.send(result);
         });
-        app.get('/products', async(req,res)=>{
-            const result= await productCollection.find().toArray();
+        app.get('/products', async (req, res) => {
+            const result = await productCollection.find().toArray();
             res.send(result);
         });
+        // cartProducts
+        app.post('/cartProducts', async (req, res) => {
+            const product = req.body;
+            const result = await cartProductCollection.insertOne(product);
+            res.send(result);
+        });
+        app.get('/cartProducts', async (req, res) => {
+            const result = await cartProductCollection.find().toArray();
+            res.send(result);
+        });
+        // Update:
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        });
+        app.put('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const data = req.body;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedProduct = {
+                $set: {
+                    photo: data.photo,
+                    name: data.name,
+                    brand_name: data.brand_name,
+                    price: data.price,
+                    type: data.type,
+                    description: data.description,
+                    rating: data.rating
+                },
+            };
+            const result = await productCollection.updateOne(filter, updatedProduct, options);
+            res.send(result);
+        })
 
 
 
