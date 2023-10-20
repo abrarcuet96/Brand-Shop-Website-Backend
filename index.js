36d8d7e3cb4f13993875a11b0762b14d8fa79ba5
abrarcuet96 <133400726+abrarcuet96@users.noreply.github.com>
2023-10-20 18:01:05 +0600

    pass and user protected

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ app.use(cors());
 app.use(express.json());
 
 
-// abrarhaider1357
-// UCiQ9fa8AWOPench
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://abrarhaider1357:UCiQ9fa8AWOPench@cluster0.0zifi4k.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0zifi4k.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
